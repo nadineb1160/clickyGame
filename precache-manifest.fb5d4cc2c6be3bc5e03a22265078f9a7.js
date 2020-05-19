5416d6fe5836ff22ce9b758203bd7199bd544ebc
nadineb <nadineb@umich.edu>
2020-05-19 09:57:26 -0700

    Updates

---
+++ precache-manifest.fb5d4cc2c6be3bc5e03a22265078f9a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eca850b1486bc5ba54c8dd7c1995cf1c",
+    "url": "/clickyGame/index.html"
+  },
+  {
+    "revision": "407ef26b48f741e07cb1",
+    "url": "/clickyGame/static/css/main.927b5723.chunk.css"
+  },
+  {
+    "revision": "19a100ef3f6538859f40",
+    "url": "/clickyGame/static/js/2.5eafe7ea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clickyGame/static/js/2.5eafe7ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "407ef26b48f741e07cb1",
+    "url": "/clickyGame/static/js/main.046fd0ec.chunk.js"
+  },
+  {
+    "revision": "33b77d3bdf984ff1f275",
+    "url": "/clickyGame/static/js/runtime-main.f8f54243.js"
+  }
+]);
